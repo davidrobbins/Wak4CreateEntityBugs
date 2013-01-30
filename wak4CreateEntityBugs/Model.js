ab9aed8ba3d901e5115c63f7cbcbe6f1f7b62f92
David Robbins <dave@wakanda.org>
2013-01-30 08:46:08 -0800

    initial commit.

---
+++ wak4CreateEntityBugs/Model.js
@@ -0,0 +1,22 @@
+﻿
+guidedModel =// @startlock
+{
+	Car :
+	{
+		methods :
+		{// @endlock
+			newCar:function()
+			{// @lock
+				return new ds.Car();
+			}// @startlock
+		},
+		events :
+		{
+			onInit:function()
+			{// @endlock
+				this.doors = 2;
+				this.wheels = 4;
+			}// @startlock
+		}
+	}
+};// @endlock
